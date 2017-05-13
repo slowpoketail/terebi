e8f7d4935964b42db4a5e836ef5dfa810bfea765
slowpoke <mail+git@slowpoke.io>
2017-05-13 18:53:49 +0000

    Add __init__.py to terebi.

---
+++ terebi/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# terebi - a python interface to mpv
+#
+# Author: slowpoke <mail+git@slowpoke.io>
+#
+# This program is Free Software under the non-terms
+# of the Anti-License. Do whatever the fuck you want.
+
